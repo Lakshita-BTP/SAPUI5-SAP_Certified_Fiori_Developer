6a76752f3b14eabd667aaf42bfa2f431b1b6541c
Jhony Gautam <lakshita.flow@gmail.com>
2026-08-20 08:07:36 +0000

    03. Defining and Using Modules

---
+++ sapui5-development-learning-journey/webapp/index.js
@@ -0,0 +1,11 @@
+sap.ui.define(["sap/m/Text"], function (Text) {
+  "use strict";
+
+//   var oText = new Text({text: "Hello World"});
+//         oText.placeAt("content");
+
+  new Text({ text: "Hello World" }).placeAt("content");
+
+});
+
+
